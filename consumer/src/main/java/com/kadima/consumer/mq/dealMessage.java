1dee761e1b00011f29a7d678e84dea538325e941
Keras-QY <103620219+Keras-QY@users.noreply.github.com>
2022-05-02 23:15:46 +0800

    RAbbit消息不丢失处理

--- consumer/src/main/java/com/kadima/consumer/mq/dealMessage.java
+++ consumer/src/main/java/com/kadima/consumer/mq/dealMessage.java
@@ -1,7 +1,11 @@
 package com.kadima.consumer.mq;
 
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * @author qianyong
@@ -13,7 +17,18 @@ import org.springframework.stereotype.Component;
 public class dealMessage {
 
     @RabbitListener(queues = "${study.rabbitMQ.queue}")
-    public void delMes(String mes){
+    public void delMes(String mes, Message message, Channel channel) throws IOException {
         System.out.println("接收到消息："+mes);
+        long deliveryTag = message.getMessageProperties().getDeliveryTag();
+
+        /**
+         * 消息被正确处理,手动ack机制
+         * 消费异常，把消息重新放回队列给其他消费者或者下次使用：
+         * channel.basicNack(deliveryTag, false, true)，或者 channel.basicReject(deliveryTag:, true);
+         * 消费异常，不想把消息重新放回队列
+         * channel.basicNack(deliveryTag, false, false）, channel.basicReject(deliveryTag:, false);
+         */
+        channel.basicAck(deliveryTag,false);
+
     }
 }
--- producer/src/main/java/com/kadima/producer/config/controller/sendMessage.java
+++ producer/src/main/java/com/kadima/producer/config/controller/sendMessage.java
@@ -1,10 +1,11 @@
 package com.kadima.producer.config.controller;
 
 import com.kadima.producer.config.RabbitmqConfig;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -23,6 +24,19 @@ public class sendMessage {
     @RequestMapping("/send")
     public void sendmes(){
         System.out.println("进入到发送消息控制层");
+        rabbitTemplate.setConfirmCallback((CorrelationData correlationData, boolean b, String s) -> {
+
+                if (b){
+                    System.out.println("消息发送成功");
+                }else {
+                    System.out.println("消息发送失败，重新发送");
+                }
+
+        });
+
+        rabbitTemplate.setReturnCallback((Message message, int replyCode, String replyText, String exchange, String routingKey) ->{
+            System.out.println("消息发送到队列成功");
+        });
         rabbitTemplate.convertAndSend(RabbitmqConfig.Exchange_Bean,"5a751fab6abb5044e0d19ea1","hello world");
     }
 }
