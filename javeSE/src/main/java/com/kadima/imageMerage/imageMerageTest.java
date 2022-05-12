7b11dbb977e0853d41659426afb78f56e8ab7876
Keras-QY <103620219+Keras-QY@users.noreply.github.com>
2022-05-12 22:32:07 +0800

    合成图片

---
+++ javeSE/src/main/java/com/kadima/imageMerage/imageMerageTest.java
@@ -0,0 +1,33 @@
+package com.kadima.imageMerage;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author qianyong
+ * @company finedo.cn
+ * @date 2022/5/12
+ * @title：
+ */
+public class imageMerageTest {
+
+    public static void main(String[] args) {
+       /* *//*File file1 = new File();
+        File file2 = new File();
+    }*//*
+
+
+    private boolean merageImage(String path1,String path2) throws IOException {
+        //图片的地址
+
+
+        //图片转化为BufferedImage对象
+        BufferedImage[] images = new BufferedImage[2];
+        images[0] = ImageIO.read(new File(path1));
+        images[1] = ImageIO.read(new File(path2));
+*/
+
+    }
+}
