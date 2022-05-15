a8c2ec69088b9640e815b97fc5a4aa66a1dd685b
qianyong <1170632064@qq.com>
2022-05-15 22:42:42 +0800

    stream

--- javeSE/src/main/java/com/kadima/chainMode/test.java
+++ javeSE/src/main/java/com/kadima/chainMode/test.java
@@ -1,7 +1,11 @@
 package com.kadima.chainMode;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * @author qianyong
@@ -12,16 +16,50 @@ import java.util.List;
 public class test {
 
     public static void main(String[] args) {
-        com.kadima.chainMode.student student = com.kadima.chainMode.student.builder().age(28).name("钱勇").school("兰州大学").build();
-        System.out.println(student.toString());
+        //com.kadima.chainMode.student student = com.kadima.chainMode.student.builder().age(28).name("钱勇").school("兰州大学").build();
+        //System.out.println(student.toString());
 
         //stream流式计算
         student student1 = new student("小明", 20, "清华大学");
-        student student2 = new student("小明", 22, "北京大学");
-        student student3 = new student("小明", 25, "兰州大学");
-        List<com.kadima.chainMode.student> studentList = Arrays.asList(student1, student2, student3);
-        studentList.stream().filter((u) -> {
-            return u.getAge() == 25;
-        }).forEach(System.out::println);
+        student student2 = new student("小红", 27, "北京大学");
+        student student3 = new student("小强", 26, "兰州大学");
+        student student4 = new student("小文", 28, "宿州大学");
+        List<com.kadima.chainMode.student> studentList = Arrays.asList(student1, student2, student3,student4);
+
+        Predicate<student> predicate1 = student -> student.getAge() == 25;
+        Predicate<student> predicate2 = student -> student.getAge() > 25;
+        studentList.stream().filter(predicate1.or(predicate2)).collect(Collectors.toList()).forEach(test::getstring);
+
+
+        //filter过滤器
+        studentList.stream().filter( (u -> u.getAge() == 25)).forEach(System.out::println);
+
+
+
+        //收集器,生成新的集合
+        Set<com.kadima.chainMode.student> collect = studentList.stream().filter((u) -> u.getAge() > 25).collect(Collectors.toSet());
+        System.out.println(collect);
+
+        //limit
+         studentList.stream().limit(2).forEach(student -> System.out.println("limit"+student));
+
+        List<Integer> integerList = Arrays.asList(1, 3, 6, 8, 4);
+        //sorted
+        integerList.stream().sorted().forEach(System.out::println);
+        //反序
+        integerList.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+        //max
+        System.out.println(integerList.stream().max(Integer::compareTo).get());
+        //count
+        System.out.println(integerList.stream().count());
+        //map 对取出的数据进行额外的操作
+        studentList.stream().map(student -> student.getAge()+1).forEach(System.out::println);
+        //reduce
+        //studentList.stream().reduce((sum,num) -> sum+num).get()
+
+    }
+
+    private static void getstring(student ss){
+        System.out.println("===="+ss);
     }
 }
